4c76c5b93cd8db4306219ceb7ebda07b0e47d575
Atharv <atharvinfo1811@gmail.com>
2025-05-21 12:33:11 +0530

    day 7 made hangman

---
+++ day7/hangman.py
@@ -0,0 +1,33 @@
+import random
+from words import word_list
+from hangmanimages import stages
+
+word = random.choice(word_list)
+guessed = 0
+hangman = 6
+placeholderlist = []
+for i in word:
+    placeholderlist.append("_")
+placeholder = "".join(placeholderlist)
+print(placeholder)
+
+while guessed<len(word) and hangman>0:
+    guess = input("Guess a letter: ").lower()
+    prevguess = guessed
+    for i in range(len(word)):
+        if word[i] == guess:
+            placeholderlist[i] = guess
+            guessed += 1
+
+    if prevguess == guessed:
+        hangman-=1
+
+    print(stages[hangman])
+
+    placeholder = "".join(placeholderlist)
+    print(placeholder)
+
+if guessed == len(word):
+    print("You won!")
+else:
+    print(f"You lost, the word was {word}")
