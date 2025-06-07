df43c7ffd7431831441d2bf946014107f935ad8e
Atharv <atharvinfo1811@gmail.com>
2025-06-07 23:51:50 +0530

    day24

--- day24/mailmerge/main.py
+++ day24/mailmerge/main.py
@@ -1,8 +1,6 @@
 with open("Input/Letters/starting_letter.txt") as docs:
     starting_letter = docs.read()
 
-print(starting_letter)
-
 with open("Input/Names/invited_names.txt") as namesdocs:
     namecontent = namesdocs.readlines()
 
