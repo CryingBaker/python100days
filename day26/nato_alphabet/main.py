452a696a3bb0e35c0fc64a7c200fca2bc04cac20
Atharv <atharvinfo1811@gmail.com>
2025-06-09 22:43:15 +0530

    day26

---
+++ day26/nato_alphabet/main.py
@@ -0,0 +1,11 @@
+import pandas
+
+nato_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+nato_alphabet_dict = {phonetic["letter"]:phonetic["code"] for (index,phonetic) in nato_alphabet.iterrows()}
+
+user_string = input("Enter your string:").upper()
+
+string_to_nato = [nato_alphabet_dict[character] for character in user_string]
+
+print(string_to_nato)
